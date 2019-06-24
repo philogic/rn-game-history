000ca4d143f061647022799c6fab560649eb1c5a
Jakub Oblizajek <jakub.oblizajek@gmail.com>
2019-06-24 19:39:19 +0200

    Add firebase SDK and configure it in the project. Change App component from function to state-based component.

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import { firebaseConfig } from './config';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+export default class App extends React.Component<> {
+  constructor(props) {
+    super(props)
+    this.itemsDb = firebaseApp.database().ref('/Games');
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
+const styles = {
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+};
+
---
+++ config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAD8ueqe9xJwOqdhETRndWnfvafakZS734",
+  authDomain: "rn-game.firebaseapp.com",
+  databaseURL: "https://rn-game.firebaseio.com",
+  projectId: "rn-game",
+  storageBucket: "rn-game.appspot.com",
+  messagingSenderId: "780396349143",
+  appId: "1:780396349143:web:beb84adc72949fc9"
+};
+
+export default firebaseConfig;
